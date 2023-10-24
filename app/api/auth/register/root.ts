4f8f919d6a209e7f550c83c95a1ebcaf0482076a
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-10-24 17:21:34 +0200

    Refractor the code to use client in form.tsx

--- app/api/auth/register/root.ts
+++ app/api/auth/register/root.ts
@@ -2,7 +2,7 @@ export default async function POST(request: Request){
     try{
         const { email, password } = await request.json();
         // validate email and password also we can use popular package of zod
-        console.log({ email, password })
+        console.log({ email, password });
     }
     catch(e){
         console.log({ e })
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,28 +1,8 @@
-import { FormEvent } from "react";
+import Form from "./form";
 
 const RegisterPage = () => {
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const formData = new FormData(e.currentTarget);
-        const response =  await fetch(`/api/auth/register`, {
-            method: 'POST',
-            body: JSON.stringify({
-                email: formData.get('email'),
-                password: formData.get('password')
-            }),
-
-        });
-        console.log({ response })
-
-    }
-
-
     return (
-        <form onSubmit={handleSubmit} className="flex flex-col gap-2 mx-auto max-w-md mt-10">
-            <input className="border border-black" type="email" />
-            <input  className="border border-black" type="password" />
-            <button type="submit">Register</button>
-        </form>
+        <Form /> 
     )
 }
 
