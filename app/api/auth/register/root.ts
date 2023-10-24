a7b6d732238a67f4035f491e6424d3e075bd5751
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-10-24 17:11:28 +0200

    Setup next auth

---
+++ app/api/auth/register/root.ts
@@ -0,0 +1,10 @@
+export default async function POST(request: Request){
+    try{
+        const { email, password } = await request.json();
+        // validate email and password also we can use popular package of zod
+        console.log({ email, password })
+    }
+    catch(e){
+        console.log({ e })
+    }
+}
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,24 @@
+import { FormEvent } from "react";
+
 const RegisterPage = () => {
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const response =  await fetch(`/api/auth/register`, {
+            method: 'POST',
+            body: JSON.stringify({
+                email: formData.get('email'),
+                password: formData.get('password')
+            }),
+
+        });
+        console.log({ response })
+
+    }
+
+
     return (
-        <form className="flex flex-col gap-2 mx-auto max-w-md mt-10">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2 mx-auto max-w-md mt-10">
             <input className="border border-black" type="email" />
             <input  className="border border-black" type="password" />
             <button type="submit">Register</button>
