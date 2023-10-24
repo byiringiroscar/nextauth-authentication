077b5cffdad830e9852ee7edde34a69b095ba328
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-10-24 16:42:18 +0200

    Update page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,6 @@
 const RegisterPage = () => {
     return (
-        <form className="flex flex-col gap-2">
+        <form className="flex flex-col gap-2 mx-auto max-w-md mt-10">
             <input className="border border-black" type="email" />
             <input  className="border border-black" type="password" />
             <button type="submit">Register</button>
