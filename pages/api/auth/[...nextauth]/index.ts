c234e6e4e480ee717167d914646315d6cfe62d26
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-02 22:59:53 +0200

    Update index.ts

--- pages/api/auth/[...nextauth]/index.ts
+++ pages/api/auth/[...nextauth]/index.ts
@@ -8,7 +8,6 @@ import type { NextAuthOptions } from "next-auth"
 import { getServerSession } from "next-auth"
 
 
-
 const handler = NextAuth({
   session: {
     strategy: 'jwt',
@@ -45,3 +44,45 @@ const handler = NextAuth({
 
 
 export default handler;
+
+// =======================  this can work also ================================
+// export const config = {
+//   session: {
+//     strategy: 'jwt',
+//   },
+//   providers: [
+//     CredentialsProvider({
+//         credentials: {
+//             email: {},
+//             password: {}
+//           },
+//           async authorize(credentials, req) {
+//             // Add logic here to look up the user from the credentials supplied
+//             const response = await sql`SELECT * FROM users WHERE email = ${credentials?.email}`;
+//             const user = response.rows[0];
+//             const passwordCorrect = await compare(
+//               credentials?.password || '',
+//               user.password
+//             );
+//             console.log({passwordCorrect})
+//             if(passwordCorrect){
+//               return {
+//                 id: user.id,
+//                 email: user.email
+//               }
+//             }
+//             console.log({credentials})
+//             return null
+            
+//           }
+//     })
+// ]
+// } satisfies NextAuthOptions
+
+// // Use it in server contexts
+// export function auth(...args: [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]] | [NextApiRequest, NextApiResponse] | []) {
+//   return getServerSession(...args, config)
+// }
+
+
+// export default NextAuth(config);
