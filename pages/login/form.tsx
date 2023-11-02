eb11834f981e3c159eba132b9139cb115d28e8f6
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-02 23:16:52 +0200

    Update redirect after login

--- pages/login/form.tsx
+++ pages/login/form.tsx
@@ -1,18 +1,24 @@
 'use client';
 import { signIn } from "next-auth/react";
 import { FormEvent } from "react";
+import { useRouter } from "next/navigation";
 
 
 const Form = () => {
+    const router = useRouter();
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
         const response = await signIn('credentials', {
             email: formData.get('email'),
             password: formData.get('password'),
-            redirect: false
+            redirect: false,
         });
         console.log({ response })
+        if(!response?.error){
+            router.push('/');
+            // router.refresh();
+        }
     }
     return (
         <form onSubmit={handleSubmit} className="flex flex-col gap-2 mx-auto max-w-md mt-10">
